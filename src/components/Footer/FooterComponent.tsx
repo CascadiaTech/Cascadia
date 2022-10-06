d883b0f0ba26aec718889808c786b53b4bc9953b
Conner <conner_holloway@xplornet.ca>
2022-10-06 09:47:29 -0700

    footer.text.edit

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -223,95 +223,12 @@ export default function FooterComponent() {
                   </li>
                 </ul>
               </div>
-
-              <div className="mb-6">
-                <h5 className="uppercase font-bold mb-2.5">Links</h5>
-
-                <ul className="list-none mb-0">
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 1
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 2
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 3
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 4
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-              <div className="mb-6">
-                <h5 className="uppercase font-bold mb-2.5">Links</h5>
-
-                <ul className="list-none mb-0">
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 1
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 2
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 3
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 4
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-              <div className="mb-6">
-                <h5 className="uppercase font-bold mb-2.5">Links</h5>
-
-                <ul className="list-none mb-0">
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 1
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 2
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 3
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!" className="text-white">
-                      Link 4
-                    </a>
-                  </li>
-                </ul>
-              </div>
             </div>
           </div>
 
           <div className="text-center p-4">
             © 2021 Copyright:
-            <a className="text-white" href="https://tailwind-elements.com/">
-              Tailwind Elements
-            </a>
+            <p>CascadiaTech</p>
           </div>
         </footer>
       </div>
